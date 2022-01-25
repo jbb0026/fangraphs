d1d5986924f506b19dce6ad70d40be3be8042008
Joseph <jbb0026@mix.wvu.edu>
2022-01-25 15:10:05 -0500

    Implemented header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { TopnavbarComponent } from 'src/app/navigation/topnavbar.component';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './navigation/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopnavbarComponent
+    TopnavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
